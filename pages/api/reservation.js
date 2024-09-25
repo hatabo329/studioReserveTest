2f9c27b8e48f1ede4cd1d03b68fbaa79d6949633
hatabo329 <hatabo329@gmail.com>
2024-09-26 00:25:55 +0900

    微修正

--- pages/api/reservation.js
+++ pages/api/reservation.js
@@ -33,8 +33,9 @@ export default async function handler(req, res) {
           });
 
       res.status(200).json({ message: 'Reservation successful' });
-    } catch {
-      res.status(500).json({ error: 'Error adding reservation' });
+    } catch (error) {
+        console.error('Error appending to spreadsheet:', error);  // エラー内容をコンソールに出力
+        res.status(500).json({ error: 'Error adding reservation' });
     }
   } else {
     res.status(405).json({ message: 'Method Not Allowed' });
